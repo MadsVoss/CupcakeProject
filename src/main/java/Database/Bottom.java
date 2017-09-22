5fb58dd3ffaa8b103f6381deb943afa1f6a25dbe
JonatanMagnusBakke <31619536+JonatanMagnusBakke@users.noreply.github.com>
2017-09-22 13:01:18 +0200

    awd

---
+++ src/main/java/Database/Bottom.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+/**
+ *
+ * @author Jonatan
+ */
+public class Bottom {
+    private String name;
+    private float price;
+
+    public Bottom(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    
+    
+}
