9535720ae2041095e5ba920ce4ace0f5ff53973b
JonatanMagnusBakke <31619536+JonatanMagnusBakke@users.noreply.github.com>
2017-09-22 13:01:09 +0200

    awd

---
+++ src/main/java/Database/Topping.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+/**
+ *
+ * @author Jonatan
+ */
+public class Topping {
+    private String name;
+    private float price;
+
+    public Topping(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    
+}
